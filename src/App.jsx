86e3a8b481c1c743f0acd9992b4e57e6cec99e57
Bhawesh <bhaweshjoshi689@gmail.com>
2025-12-19 03:56:53 +0530

    added the text-box with upper case funcn

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import Textbox from '../../components/Textbox';
+import {useState} from 'react'
+
+function App() {
+  const[color,setColor] = useState('white');
+ 
+  const togglemode = ()=>{
+    if(color==="white")setColor('black');
+    else setColor('white');
+  }
+  return (
+    <>
+    <Navbar mode = {color} togglemode ={togglemode}/>
+    <Textbox heading ="Enter Text To Analyze"/>
+
+    </>
+
+  )
+}
+
+export default App
