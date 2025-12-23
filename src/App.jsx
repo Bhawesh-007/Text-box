ef95f9590a96aacc43ccfdad81816912d8175209
Bhawesh <bhaweshjoshi689@gmail.com>
2025-12-23 20:01:35 +0530

    refactored paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../../components/Navbar'
-import Textbox from '../../components/Textbox';
+import Navbar from '/components/Navbar'
+import Textbox from '/components/Textbox';
 import './App.css'
 import {useState} from 'react'
 
